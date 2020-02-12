77aee93c159fc609cd3c40bb1f03bced4abb1965
formacio <formacio@BARCELO-EHVPRGP>
2020-02-12 12:51:26 +0100

    Project Created and Fase done.

---
+++ src/Fase1.java
@@ -0,0 +1,15 @@
+
+public class Fase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char charName[]= {'P','E','D','R','O'};
+		
+		for (int i=0; i<charName.length; i++) {
+			
+			System.out.println(charName[i]);
+		}
+			
+	}
+
+}
